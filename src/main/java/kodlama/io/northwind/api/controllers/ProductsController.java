f23e02d919adbc7824f52d50f35702aa566a1f12
Hüseyin Doğukan Yılmaz <dogu_codeM@hotmail.com>
2021-05-23 13:32:59 +0300

    add crud

--- src/main/java/kodlama/io/northwind/api/controllers/ProductsController.java
+++ src/main/java/kodlama/io/northwind/api/controllers/ProductsController.java
@@ -4,10 +4,14 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import kodlama.io.northwind.business.abstracts.ProductService;
+import kodlama.io.northwind.core.utilities.results.DataResult;
+import kodlama.io.northwind.core.utilities.results.Result;
 import kodlama.io.northwind.entities.concretes.Product;
 
 @RestController
@@ -23,8 +27,19 @@ public class ProductsController {
 	}
 	
 	@GetMapping("/getall")
-	public List<Product> getAll() {
+	public DataResult<List<Product>> getAll() {
 		return this.productService.getAll();
 	}
+	
+	@PostMapping("/add")
+	public Result add(@RequestBody Product product) {
+		return this.productService.add(product);
+	}
+	
+	@PostMapping("/delete")
+	public Result delete(@RequestBody Product product) {
+		return this.productService.delete(product);
+	}
+	
 	
 }
--- src/main/java/kodlama/io/northwind/business/concretes/ProductManager.java
+++ src/main/java/kodlama/io/northwind/business/concretes/ProductManager.java
@@ -6,6 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import kodlama.io.northwind.business.abstracts.ProductService;
+import kodlama.io.northwind.core.utilities.results.DataResult;
+import kodlama.io.northwind.core.utilities.results.Result;
+import kodlama.io.northwind.core.utilities.results.SuccessDataResult;
+import kodlama.io.northwind.core.utilities.results.SuccessResult;
 import kodlama.io.northwind.dataAccess.abstracts.ProductDao;
 import kodlama.io.northwind.entities.concretes.Product;
 
@@ -21,10 +25,28 @@ public class ProductManager implements ProductService{
 	}
 
 	@Override
-	public List<Product> getAll() {
+	public DataResult<List<Product>> getAll() {
 		
-		return this.productDao.findAll();
+		return new SuccessDataResult<List<Product>>(this.productDao.findAll(),"Data listelendi");
 		
+	}
+
+	@Override
+	public Result add(Product product) {
+		this.productDao.save(product);
+		return new SuccessResult("Ürün eklendi");
+	}
+
+	@Override
+	public Result delete(Product product) {
+		this.productDao.delete(product);
+		return new SuccessResult("Ürün silindi");
+	}
+
+	@Override
+	public Result update(Product product) {
+		
+		return null;
 	}
 
 }
