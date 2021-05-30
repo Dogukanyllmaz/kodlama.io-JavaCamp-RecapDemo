fe4f195b13599a0e1a8a660d09af6e1348da08ec
Hüseyin Doğukan Yılmaz <dogu_codeM@hotmail.com>
2021-05-30 20:51:53 +0300

    ders 9

--- src/main/java/kodlama/io/northwind/api/controllers/ProductsController.java
+++ src/main/java/kodlama/io/northwind/api/controllers/ProductsController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import kodlama.io.northwind.business.abstracts.ProductService;
@@ -15,7 +16,7 @@ import kodlama.io.northwind.core.utilities.results.Result;
 import kodlama.io.northwind.entities.concretes.Product;
 
 @RestController
-@RequestMapping("/api/products")
+@RequestMapping("/api/products/")
 public class ProductsController {
 
 	private ProductService productService;
@@ -26,19 +27,64 @@ public class ProductsController {
 		this.productService = productService;
 	}
 	
-	@GetMapping("/getall")
+	@GetMapping("getall")
 	public DataResult<List<Product>> getAll() {
 		return this.productService.getAll();
 	}
 	
-	@PostMapping("/add")
+	@GetMapping("getAllByPage")
+	public DataResult<List<Product>> getAll(int pageNo, int pageSize) {
+		return this.productService.getAll(pageNo, pageSize);
+	}
+	
+	@PostMapping("add")
 	public Result add(@RequestBody Product product) {
 		return this.productService.add(product);
 	}
 	
-	@PostMapping("/delete")
+	@PostMapping("delete")
 	public Result delete(@RequestBody Product product) {
 		return this.productService.delete(product);
+	}
+	
+	@GetMapping("getByProductName")
+	public DataResult<Product> getByProductName(@RequestParam String productName) {
+		return this.productService.getByProductName(productName);
+	}
+	
+	@GetMapping("getByProductNameAndCategoryId")
+	public DataResult<Product> getByProductNameAndCategoryId(@RequestParam("productName") String productName,@RequestParam("categoryId") int categoryId) {
+		return this.productService.getByProductNameAndCategoryId(productName, categoryId);
+	}
+	
+	@GetMapping("getByProductNameOrCategory")
+	public DataResult<List<Product>> getByProductNameOrCategory(@RequestParam String productName,@RequestParam int categoryId) {
+		return this.productService.getByProductNameOrCategory(productName, categoryId);
+	}
+	
+	@GetMapping("getByCategoryIdIn")
+	public DataResult<List<Product>> getByCategoryIdIn(@RequestParam List<Integer> categories){
+		return this.productService.getByCategoryIdIn(categories);
+	}
+	
+	@GetMapping("getByProductNameContains")
+	public DataResult<List<Product>> getByProductNameContains(@RequestParam String productName) {
+		return this.productService.getByProductNameContains(productName);
+	}
+	
+	@GetMapping("getByProductNameStartsWith")
+	public DataResult<List<Product>> getByProductNameStartsWith(@RequestParam String productName) {
+		return this.productService.getByProductNameStartsWith(productName);
+	}
+	
+	@GetMapping("getByNameAndCategory")
+	public DataResult<List<Product>> getByNameAndCategory(@RequestParam String productName,@RequestParam int categoryId) {
+		return this.productService.getByNameAndCategory(productName, categoryId);
+	}
+	
+	@GetMapping("getAllDesc")
+	public DataResult<List<Product>> getAllSorted() {
+		return this.productService.getAllSorted();
 	}
 	
 	
--- src/main/java/kodlama/io/northwind/business/abstracts/ProductService.java
+++ src/main/java/kodlama/io/northwind/business/abstracts/ProductService.java
@@ -2,6 +2,7 @@ package kodlama.io.northwind.business.abstracts;
 
 import java.util.List;
 
+
 import kodlama.io.northwind.core.utilities.results.DataResult;
 import kodlama.io.northwind.core.utilities.results.Result;
 import kodlama.io.northwind.entities.concretes.Product;
@@ -9,8 +10,31 @@ import kodlama.io.northwind.entities.concretes.Product;
 public interface ProductService {
 
 	DataResult<List<Product>> getAll();
+	
+	DataResult<List<Product>> getAll(int pageNo, int pageSize);
+	
+	DataResult<List<Product>> getAllSorted();
+	
 	Result add(Product product);
+	
 	Result delete(Product product);
+	
 	Result update(Product product);
+	
+	DataResult<Product> getByProductName(String productName);
+	
+	DataResult<Product> getByProductNameAndCategoryId(String productName, int categoryId);
+	
+	DataResult<List<Product>> getByProductNameOrCategory(String productName, int categoryId);
+	
+	DataResult<List<Product>> getByCategoryIdIn(List<Integer> categories);
+	
+	DataResult<List<Product>> getByProductNameContains(String productName);
+	
+	DataResult<List<Product>> getByProductNameStartsWith(String productName);
+	
+	DataResult<List<Product>> getByNameAndCategory(String productName, int categoryId);
+	
+	
 	
 }
